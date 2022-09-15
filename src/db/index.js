fb0237a0446d104f9444cb6877a5a89294d1fa31
Guzpon357 <guzman.ponce.leo@gmail.com>
2022-09-15 16:58:12 -0500

    Primer commit

---
+++ src/db/index.js
@@ -0,0 +1,35 @@
+const dataClientes = [
+    {
+        name:"Leonardo Guzmán",
+        edad:"21",
+        direccion:"Ate 301"
+    },
+    {
+        name:"Carmen Rosa" ,
+        edad:"54",
+        direccion:"Ate 301"
+    },
+    {
+        name:"Alejandro",
+        edad:"27",
+        direccion:"Colonial 3130"
+    }
+];
+
+const dataProductos = [
+    {
+        nombre:"Monitor",
+        precio:"699.99",
+        stock:"10"
+    },
+    {
+        nombre:"HeadPhones",
+        precio:"389.99",
+        stock:"20"
+    },
+    {
+        nombre:"PS4",
+        precio:"1399.99",
+        stock:"10"
+    }
+];
